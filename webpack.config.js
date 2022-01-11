10572fd3750bca1573a1910c1c18d95af4df4dc5
gtekle <teklegy@gmail.com>
2022-01-11 13:00:28 +0300

    Set index.html as template and add webpack.config.js config file

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Custom template',
+      template: 'index.html'
+    })
+  ],
+};
