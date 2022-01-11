e659e46868ab6ad277ae7bc17ea5baa1c073549c
gtekle <teklegy@gmail.com>
2022-01-11 18:28:42 +0300

    Add styling and refactor onLoad event handler code into functions

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
+import './style.css';
+
 const tasks = [
   { description: 'Task1', isCompleted: false, index: 1 },
   { description: 'Task2', isCompleted: false, index: 2 },
   { description: 'Task3', isCompleted: false, index: 3 },
   { description: 'Task4', isCompleted: false, index: 4 },
   { description: 'Task5', isCompleted: false, index: 5 },
-  { description: 'Task6', isCompleted: false, index: 6 },
+  { description: 'Task6', isCompleted: true, index: 6 },
   { description: 'Task7', isCompleted: false, index: 7 },
   { description: 'Task8', isCompleted: false, index: 8 },
   { description: 'Task9', isCompleted: false, index: 9 },
@@ -12,23 +14,59 @@ const tasks = [
 
 const createTaskItem = (task) => {
   const taskItem = document.createElement('li');
-  const form = document.createElement('form');
-  form.innerHTML = '<input type="checkbox" name="isCompleted" value=${task.isCompleted}/>';
-  const taskDescription = document.createElement('p');
-  taskDescription.textContent = task.description;
-
+  taskItem.innerHTML = `<div class="item-description"><form id="form-is-completed"> 
+    <input type="checkbox" name="isCompleted" value=${task.isCompleted}/></form>
+    <p>${task.description}</p> </div>
+    <i class="fa fa-ellipsis-v"></i>`
   taskItem.id = task.index;
-  taskItem.appendChild(form);
-  taskItem.appendChild(taskDescription);
 
   return taskItem;
+}
+
+const todoListHeading = () => {
+  const todoHeading = document.createElement('li');
+  todoHeading.innerHTML = `<h1> Today's To Do </h1 >
+    <button type='button' class='btn btn-refresh'>
+    <i class="fa fa-refresh"></i></button>`
+  todoHeading.classList.add('todo-heading');
+
+  return todoHeading;
+}
+
+const formAddTask = () => {
+  const form = document.createElement('form');
+  form.id = 'form-add-task';
+  form.innerHTML = '<input type="text" name="new-task" placeholder="Add task ..."/>';
+
+  const btnAddTask = document.createElement('button');
+  btnAddTask.setAttribute('type', 'button');
+  btnAddTask.setAttribute('name', 'btnAddNewTask');
+  btnAddTask.innerText = 'Add';
+  btnAddTask.classList.add('btn', 'btn-add-task');
+  form.appendChild(btnAddTask);
+
+  return form;
+}
+
+const btnClearAllCompleted = () => {
+  const btnClearCompleted = document.createElement('button');
+  btnClearCompleted.setAttribute('type', 'button');
+  btnClearCompleted.setAttribute('name', 'btnClearCompleted');
+  btnClearCompleted.innerText = 'Clear all completed';
+  btnClearCompleted.classList.add('btn', 'btn-clear-completed');
+
+  return btnClearCompleted;
 }
 
 window.addEventListener('load', () => {
   const todoList = document.querySelector('.todo-list');
 
+  todoList.appendChild(todoListHeading());
+  todoList.appendChild(formAddTask());
+
   tasks.forEach(task => {
     todoList.appendChild(createTaskItem(task));
   })
 
+  todoHeading.parentNode.appendChild(btnClearAllCompleted());
 });
--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,14 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Custom template',
