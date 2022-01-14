ad94b1c5a5c28eabb68ddc811a25f0eb7faded47
gtekle <teklegy@gmail.com>
2022-01-15 01:24:12 +0300

    Add image and font asset loaders to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,14 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
   devServer: {
