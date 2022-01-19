7e0253decb36d83831334a29cbc79c0ea743e6d6
OybekKayumov <oybek.kayumov@gmail.com>
2022-01-19 19:26:00 +0500

    add render list item test

--- src/modules/Task.test.js
+++ src/modules/Task.test.js
@@ -1,18 +1,35 @@
 import Task from './Task.js';
 import DataStore from './DataStore.js';
 import 'jest-localstorage-mock';
+import renderTaskItem from '../components/TaskListItem.js';
+import btnClearAllCompleted from '../components/ClearAllButton.js';
 
 DataStore.tasks = [];
 
-const task = new Task({ description: "task one", isCompleted: false, index: 1 });
+const task = new Task({ description: 'task one', isCompleted: false, index: 1 });
 
-describe("Task CRUD", () => {
-  test("add task", () => {
+describe('add Task', () => {
+  test('add method', () => {
     task.addTask(task);
     expect(DataStore.tasks.length).toBe(1);
   });
-  
-  test("removeTask", () => {
+
+  test('render Task item to the list', () => {
+    document.body.innerHTML = '<ul id="list" class="todo-list"></ul>';
+    document.body.appendChild(btnClearAllCompleted());
+    renderTaskItem(task);
+    const list = document.querySelectorAll('.task');
+    expect(list).toHaveLength(1);
+  });
+});
+
+describe('remove Task', () => {
+  test('removeTask', () => {
+    task.removeTask(task.index);
+    expect(DataStore.tasks.length).toBe(0);
+  });
+
+  test('remove Task from DOM', () => {
     task.removeTask(task.index);
     expect(DataStore.tasks.length).toBe(0);
   });
